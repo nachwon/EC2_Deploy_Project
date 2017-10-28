e5120124980f897d4d5c6f566337498c63976ff8
Che1 <nachwon@gmail.com>
2017-10-29 00:49:55 +0900

    minor refactoring

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -14,16 +14,19 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ROOT_DIR =os.path.dirname(BASE_DIR)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
+# static files
+STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+
+# media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 
